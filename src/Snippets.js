43fccada8038d191d3b4de542dd492cdc63573e0
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2018-03-20 13:09:20 +0100

    Use HTTPS version of GTM

--- src/Snippets.js
+++ src/Snippets.js
@@ -14,7 +14,7 @@ const Snippets = {
       (function(w,d,s,l,i){w[l]=w[l]||[];
         w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${JSON.stringify(events).slice(1, -1)}});
         var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-        j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;
+        j.async=true;j.src='https://googletagmanager.com/gtm.js?id='+i+dl;
         f.parentNode.insertBefore(j,f);
       })(window,document,'script','${dataLayerName}','${id}');`
   
